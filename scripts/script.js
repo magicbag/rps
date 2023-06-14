a6056592dbde7e45a783cacd9769b3dd9e6e44fb
Kyle <10909409+magicbag@users.noreply.github.com>
2023-06-14 18:25:39 -0300

    Added computer choices (random) and basic game logic for 1 round of rock, paper, scissors.

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+const choice = ["Rock", "Paper", "Scissors"];
+const wonRound = "You win!";
+const lostRound = "You lose!";
+const tieRound = "You tied. Both made same choice!";
+const rockPaper = "Paper covers rock.";
+const rockScissors = "Rock crushes scissors.";
+const paperScissors = "Scissors cut paper.";
+
+function getComputerChoice() {
+        return choice[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock") {
+        if (computerSelection == "Rock") {
+            return tieRound;
+        }
+        else if (computerSelection == "Paper") {
+            return lostRound + " " + rockPaper;
+        }
+        else {
+            return wonRound + " " + rockScissors;
+        }
+    }
+    else if (playerSelection == "Paper") {
+        if (computerSelection == "Rock") {
+            return wonRound + " " + rockPaper;
+        }
+        else if (computerSelection == "Paper") {
+            return tieRound;
+        }
+        else {
+            return lostRound + " " + paperScissors;
+        }
+    }
+    else if (playerSelection == "Scissors") {
+        if (computerSelection == "Rock") {
+            return lostRound + " " + rockScissors;
+        }
+        if (computerSelection == "Paper") {
+            return wonRound + " " + paperScissors;
+        }
+        else {
+            return tieRound;
+        }
+    }
+}
