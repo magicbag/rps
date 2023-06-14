c36c45fccfbab07ca79c29fb5287db5ea6a26710
Kyle <10909409+magicbag@users.noreply.github.com>
2023-06-14 18:58:18 -0300

    Add a check for tie games in playRound to condense code

--- scripts/script.js
+++ scripts/script.js
@@ -14,37 +14,34 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection.replace(playerSelection[0], playerSelection[0].toUpperCase());
     
+    if (playerSelection === computerSelection) {
+        return tieRound;
+    }
+
     if (playerSelection == "Rock") {
-        if (computerSelection == "Rock") {
-            return tieRound;
-        }
-        else if (computerSelection == "Paper") {
+        if (computerSelection == "Paper") {
             return lostRound + " " + rockPaper;
         }
-        else {if (computerSelection == "Scissors")
+        else {
             return wonRound + " " + rockScissors;
         }
     }
+
     else if (playerSelection == "Paper") {
         if (computerSelection == "Rock") {
             return wonRound + " " + rockPaper;
-        }
-        else if (computerSelection == "Paper") {
-            return tieRound;
         }
         else {
             return lostRound + " " + paperScissors;
         }
     }
+    
     else if (playerSelection == "Scissors") {
         if (computerSelection == "Rock") {
             return lostRound + " " + rockScissors;
         }
         if (computerSelection == "Paper") {
             return wonRound + " " + paperScissors;
-        }
-        else {
-            return tieRound;
         }
     }
 }
